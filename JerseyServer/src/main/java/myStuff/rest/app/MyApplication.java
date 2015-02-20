07f0ba906e0ba7c938e60f7fbadcf9704b724b4c
Craig User <craig.kumick@gmail.com>
2015-02-19 23:57:16 -0600

    Add changes to config for XmlFile Resource (and change of formatting style).

--- JerseyServer/src/main/java/myStuff/rest/app/MyApplication.java
+++ JerseyServer/src/main/java/myStuff/rest/app/MyApplication.java
@@ -41,6 +41,7 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
@@ -49,76 +50,73 @@ import org.glassfish.jersey.servlet.ServletContainer;
 import com.wordnik.swagger.jersey.config.JerseyJaxrsConfig;
 
 public class MyApplication {
-	public static void main(String[] args) {
-		Server server = new Server();
+    public static void main(String[] args) {
+        Server server = new Server();
 
-		HttpConfiguration http_config = new HttpConfiguration();
-		http_config.setSecureScheme("https");
-		http_config.setSecurePort(8443);
-		http_config.setOutputBufferSize(32768);
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        http_config.setSecurePort(8443);
+        http_config.setOutputBufferSize(32768);
 
-		ServerConnector http = new ServerConnector(server,
-				new HttpConnectionFactory(http_config));
-		http.setPort(8080);
-		http.setIdleTimeout(30000);
+        ServerConnector http = new ServerConnector(server, new HttpConnectionFactory(http_config));
+        http.setPort(8080);
+        http.setIdleTimeout(30000);
 
-		File keystoreFile = new File(System.getProperty("user.home")
-				+ File.separator + "keystore");
-		SslContextFactory sslContextFactory = new SslContextFactory();
-		sslContextFactory.setKeyStorePath(keystoreFile.getAbsolutePath());
-		sslContextFactory.setKeyStorePassword("OBF:1xfd1zt11uha1ugg1zsp1xfp");
-		sslContextFactory.setKeyManagerPassword("OBF:1xfd1zt11uha1ugg1zsp1xfp");
+        File keystoreFile = new File(System.getProperty("user.home") + File.separator + "keystore");
+        SslContextFactory sslContextFactory = new SslContextFactory();
+        sslContextFactory.setKeyStorePath(keystoreFile.getAbsolutePath());
+        sslContextFactory.setKeyStorePassword("OBF:1xfd1zt11uha1ugg1zsp1xfp");
+        sslContextFactory.setKeyManagerPassword("OBF:1xfd1zt11uha1ugg1zsp1xfp");
 
-		HttpConfiguration https_config = new HttpConfiguration(http_config);
-		https_config.addCustomizer(new SecureRequestCustomizer());
+        HttpConfiguration https_config = new HttpConfiguration(http_config);
+        https_config.addCustomizer(new SecureRequestCustomizer());
 
-		ServerConnector https = new ServerConnector(server,
-				new SslConnectionFactory(sslContextFactory,
-						HttpVersion.HTTP_1_1.asString()),
-				new HttpConnectionFactory(https_config));
-		https.setPort(8443);
-		https.setIdleTimeout(500000);
+        ServerConnector https = new ServerConnector(server, new SslConnectionFactory(
+                sslContextFactory, HttpVersion.HTTP_1_1.asString()), new HttpConnectionFactory(
+                https_config));
+        https.setPort(8443);
+        https.setIdleTimeout(500000);
 
-		server.setConnectors(new Connector[] { https, http });
+        server.setConnectors(new Connector[] { https, http });
 
-		ServletContextHandler context = new ServletContextHandler(
-				ServletContextHandler.SESSIONS);
-		context.setContextPath("/");
-		context.addEventListener(new ShiroListener());
-		context.addEventListener(new SwaggerInitializer());
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        context.addEventListener(new ShiroListener());
+        context.addEventListener(new SwaggerInitializer());
 
-		EnumSet<DispatcherType> types = EnumSet.allOf(DispatcherType.class);
-		context.addFilter(ShiroFilter.class, "/*", types);
-		context.addFilter(ApiOriginFilter.class, "/*",
-				EnumSet.allOf(DispatcherType.class));
+        EnumSet<DispatcherType> types = EnumSet.allOf(DispatcherType.class);
+        context.addFilter(ShiroFilter.class, "/*", types);
+        context.addFilter(ApiOriginFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
 
-		ResourceConfig rc = new ResourceConfig();
-		rc.packages("myStuff.rest.resources;com.wordnik.swagger.jersey.listing")
-				.register(new MyBinder(null)).register(JacksonFeature.class)
-				.register(SseFeature.class);
+        ResourceConfig rc = new ResourceConfig();
+        rc.packages("myStuff.rest.resources;com.wordnik.swagger.jersey.listing")
+                .register(
+                        new MyBinder(new File(System.getProperty("user.home") + File.separator
+                                + "JerseyServer"))).register(MultiPartFeature.class)
+                .register(JacksonFeature.class).register(SseFeature.class);
 
-		ServletHolder h = new ServletHolder(new ServletContainer(rc));
-		h.setInitParameter(ServerProperties.PROVIDER_PACKAGES,
-				"myStuff.rest.resources;com.wordnik.swagger.jersey.listing");
-		h.setInitOrder(1);
-		context.addServlet(h, "/*");
+        ServletHolder h = new ServletHolder(new ServletContainer(rc));
+        h.setInitParameter(ServerProperties.PROVIDER_PACKAGES,
+                "myStuff.rest.resources;com.wordnik.swagger.jersey.listing");
+        h.setInitOrder(1);
+        context.addServlet(h, "/*");
 
-		// static files handler
-		ResourceHandler resourceHandler = new ResourceHandler();
-		resourceHandler.setDirectoriesListed(true);
-		resourceHandler.setResourceBase("./src/main/webapp/");
-		ContextHandler resourceContext = new ContextHandler("/api-docs-ui"); 
-		resourceContext.setHandler(resourceHandler);
+        // static files handler
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setDirectoriesListed(true);
+        resourceHandler.setResourceBase("./src/main/webapp/");
+        ContextHandler resourceContext = new ContextHandler("/api-docs-ui");
+        resourceContext.setHandler(resourceHandler);
 
-		HandlerCollection handlerList = new HandlerCollection();
-		handlerList.setHandlers(new Handler[] { resourceContext, context });
-		server.setHandler(handlerList);
+        HandlerCollection handlerList = new HandlerCollection();
+        handlerList.setHandlers(new Handler[] { resourceContext, context });
+        server.setHandler(handlerList);
 
-		server.setDumpAfterStart(true);
-		try {
-			server.start();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+        server.setDumpAfterStart(true);
+        try {
+            server.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
